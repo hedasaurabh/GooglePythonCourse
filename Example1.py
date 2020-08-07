a66197bb000c65927b8d3955935e47ed468a4ae6
heda_saurabh <heda_saurabh@yahoo.com>
2020-08-07 23:30:58 +0530

    Initial commit

---
+++ Example1.py
@@ -0,0 +1,9 @@
+Friends = ['A','B','C','D']
+print (Friends)
+for i in Friends:
+    print("Hello" + " "+ i)
+
+for i in range(0,10):
+    print("Hello World")
+for i in range(10):
+    print("Hello WW")
